9a379dc17161d9caedb9248b52a58169167153b8
sidharth-jamuar <rahuljamuar21@gmail.com>
2019-03-06 19:59:15 +0530

    new

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+}
 app.listen(PORT,()=>console.log(`Server running on PORT ${PORT}`))
