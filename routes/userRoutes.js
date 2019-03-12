6986132722cb1ab4eb61764e695d188c7e7d6799
sidharth-jamuar <rahuljamuar21@gmail.com>
2019-03-12 12:04:40 +0530

    her

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,7 +12,7 @@ module.exports=app=>{
         passport.authenticate('google'),
         (req, res) => {
             console.log(req.user)//defined
-         res.redirect("/") //not going to localhost:3000
+        // res.redirect("/") //not going to localhost:3000
           res.send({isAuth:true}) //where to handle this as in front end there is an <a> tag
         }
       );
