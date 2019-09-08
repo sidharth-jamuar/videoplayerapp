329c5e8f67d91935317b509cb1005f2563bfe745
unknown <rahuljamuar21@gmail.com>
2019-09-09 02:07:01 +0530

    routing using footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "../css/Footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHome,faSearch,faUser,faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import screenIsMobile from "../hoc/isMobile"
+import {withRouter} from "react-router-dom"
+import {connect} from "react-redux"
 const Footer=(props)=>{
+    console.log(props)
+    const isAuth=props.isAuth
     return (
         <React.Fragment>
-            {props.isMobile && <div className="footer-container"></div>}
+            {props.isMobile && <div className="footer-container">
+    
+            <FontAwesomeIcon className="icon-fawesome" icon={faHome} size="2x" color="white" onClick={e=>props.history.push("/")}/>
+            <FontAwesomeIcon className="icon-fawesome" icon={faSearch} size="2x" color="white" />
+            {isAuth &&<FontAwesomeIcon className="icon-fawesome" icon={faUser} size="2x" color="white" />}
+            {!isAuth && <FontAwesomeIcon className="icon-fawesome" icon={faUser} size="2x" color="white" onClick={e=>{props.history.push("/login")}} />}
+            <FontAwesomeIcon className="icon-fawesome" icon={faArrowLeft} size="2x" color="white" />
+            </div>}
         </React.Fragment>
          
     )
 }
-export default screenIsMobile(Footer);
+const mapStateToProps=state=>{
+    return{
+        isAuth:state.isAuth.auth
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(screenIsMobile(Footer)))
