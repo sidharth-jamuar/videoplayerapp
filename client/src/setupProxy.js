a0ec6c22d662f373233852182abaa4d6fbbc8989
Sidharth Jamuar <jsidharth@athenahealth.com>
2020-02-20 22:15:25 +0530

    removed proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(proxy('/api/*', { target: 'http://localhost:3004' }),
-  proxy("/auth/google",{target:"http://localhost:3004"}))
-};
+// module.exports = function(app) {
+//   app.use(proxy('/api/*', { target: 'http://localhost:3004' }),
+//   proxy("/auth/google",{target:"http://localhost:3004"}))
+// };
